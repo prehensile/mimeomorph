9431604edd44ffc4bf7d327616c4b9e059f69d77
Henry Cooke <me@prehensile.co.uk>
2012-10-03 11:17:50 +0100

    added MPL2 license

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 CONSUMER_KEY = "change me"
 CONSUMER_SECRET = "change me"
 DEBUG_MODE = False
